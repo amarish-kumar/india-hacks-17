3e7378afb985fa595492883962a74e3d0f766ee3
Siddharth Sahay <sahaysid@gmail.com>
2017-06-09 20:04:30 +0530

    Parsing dow

--- hotstar_segment/he_predict_segment.py
+++ hotstar_segment/he_predict_segment.py
@@ -12,9 +12,23 @@ with open(training_data_file, 'r') as fptr:
 #	test_d = json.load(fpte)
 
 train = pd.DataFrame.from_dict(train_d, orient='index')
-train.reset_index(inplace=True)
+train.reset_index(inplace=True, level=0)
 train.rename(columns = {'index':'ID'},inplace=True)
+train.replace({"segment":{"neg":"0", "pos":1}})
 print(train.head())
+#d = train["dow"]
+#print(d)
+#print(type(d))
+#train.loc[:, "dow1"] = pd.Series([{'some shit':1, "some other shit":4}]*200000, index=train.index)
+#print(train["dow1"])
+
+dow_sep = []
+for x in train["dow"]:
+	ds = {d.split(':')[0]:d.split(':')[1] for d in x.split(',')}
+	dow_sep.append(ds)
+print(dow_sep)
+	
+	
 
 
 #test = pd.DataFrame.from_dict(test_d, orient='index')
