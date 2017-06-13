2a5dc490cf48baab16c0f9bd229aea7599d6368e
Siddharth Sahay <sahaysid@gmail.com>
2017-06-13 23:54:08 +0530

    Testing on data

--- hotstar_segment/he_predict_segment.py
+++ hotstar_segment/he_predict_segment.py
@@ -1,20 +1,14 @@
-import json
+from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 
 training_data_file = "train_data.json"
 test_data_file = "test_data.json"
 output_file = "output.csv"
 
-with open(training_data_file, 'r') as fptr:
-	train_d = json.load(fptr)
-
-#with open(test_data_file, 'r') as fpte:
-#	test_d = json.load(fpte)
-
-train = pd.DataFrame.from_dict(train_d, orient='index')
+train = pd.read_json(training_data_file,orient="index")
 train.reset_index(inplace=True, level=0)
-train.rename(columns = {'index':'ID'},inplace=True)
-train.replace({"segment":{"neg":"0", "pos":1}})
+train.loc[train['segment'] == 'neg', 'segment'] = "0"
+train.loc[train['segment'] == 'pos', 'segment'] = "1"
 print(train.head())
 #d = train["dow"]
 #print(d)
@@ -26,9 +20,14 @@ dow_sep = []
 for x in train["dow"]:
 	ds = {d.split(':')[0]:d.split(':')[1] for d in x.split(',')}
 	dow_sep.append(ds)
-print(dow_sep)
-	
-	
+#print(dow_sep)
+
+target_features = train['segment'].tolist()
+#print(target_features)
+
+classifier = RandomForestClassifier(n_estimators=500,max_depth=12, max_features=10)
+classifier.fit(dow_sep, target_features)
+
 
 
 #test = pd.DataFrame.from_dict(test_d, orient='index')
